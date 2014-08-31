6aec5d9f9fa931fac57039266f3315c1691bc4dc
nickhargreaves <hargreaves1234@gmail.com>
2014-08-31 14:32:39 +0300

    image slider gallery: awesomeness :)

--- app/src/org/codeforafrica/starreports/Report_PageIndicatorActivity.java
+++ app/src/org/codeforafrica/starreports/Report_PageIndicatorActivity.java
@@ -36,13 +36,18 @@ import android.webkit.WebViewClient;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.viewpagerindicator.CirclePageIndicator;
 
-public class Report_PageIndicatorActivity extends BaseActivity{
-	
+public class Report_PageIndicatorActivity extends BaseActivity implements BaseSliderView.OnSliderClickListener{
+	private SliderLayout mDemoSlider;
 	
     
 	@Override
@@ -126,7 +131,43 @@ private void initIntroActivityList ()
      //indicator.setStrokeWidth(2 * density);
 	    		
      
-   
+     //gallery slideshow
+     mDemoSlider = (SliderLayout)findViewById(R.id.slider);
+     HashMap<String,String> url_maps = new HashMap<String, String>();
+     url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+     url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+     url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+     url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+     HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+     file_maps.put("Hannibal",R.drawable.hannibal);
+     file_maps.put("Big Bang Theory",R.drawable.bigbang);
+     file_maps.put("House of Cards",R.drawable.house);
+     file_maps.put("Game of Thrones", R.drawable.game_of_thrones);
+
+     for(String name : file_maps.keySet()){
+         TextSliderView textSliderView = new TextSliderView(this);
+         // initialize a SliderLayout
+         textSliderView
+                 .description(name)
+                 .image(file_maps.get(name))
+                 .setScaleType(BaseSliderView.ScaleType.Fit)
+                 .setOnSliderClickListener(this);
+
+         //add your extra information
+         textSliderView.getBundle()
+                 .putString("extra",name);
+
+        mDemoSlider.addSlider(textSliderView);
+     }
+     mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+     mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+     mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+     mDemoSlider.setDuration(4000);
+}
+@Override
+public void onSliderClick(BaseSliderView slider) {
+    Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
 }
 public class MyAdapter extends FragmentPagerAdapter {
 	 
